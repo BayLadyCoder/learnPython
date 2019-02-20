a6b1dc560d4d440d01c096ded5f0dbd1b9e5a75d
BayLadyCoder <rachada_32@hotmail.com>
2019-02-20 16:56:00 -0500

    Lab 1 - Datatype, Variables, Print()

---
+++ Labs/Lab1/Lab1_Part1.py
@@ -0,0 +1,13 @@
+# Name: Rachada Chairangsaris (Bay)
+# Assignment: Lab 1, Part 1
+
+firstname = input('first name: ')
+lastname = input('last name: ')
+
+print('Hello', firstname, lastname)
+print('Welcome to Python!')
+
+print('-------------------------------------------------')
+print('Name: Rachada Chairangsaris (Bay)')
+print('Assignment: Lab 1, Part 1')
+print('Date: February 1st, 2019')
